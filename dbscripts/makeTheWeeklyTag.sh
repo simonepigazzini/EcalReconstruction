5a38a891555a822971c7c272a9f4c812304ee490
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2015-10-28 19:53:36 +0100

    new run ranges

--- dbscripts/makeTheWeeklyTag.sh
+++ dbscripts/makeTheWeeklyTag.sh
@@ -1,5 +1,5 @@
 conddb_import -f sqlite_file:/afs/cern.ch/work/e/emanuele/public/ecal/pulseshapes_db/ecaltemplates_popcon_data_Run2015C_lowPU.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_weekly.db -t EcalPulseShapes_data -b 248036
-conddb_import -f sqlite_file:/afs/cern.ch/work/e/emanuele/ecal/ecalreco/multifit/studies_25ns/CMSSW_7_4_12_patch4/src/EcalReconstruction/dbscripts/ecaltemplates_popcon_runs_257394_258215.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_weekly.db -t EcalPulseShapes_data -b 257394 
-conddb_import -f sqlite_file:/afs/cern.ch/work/e/emanuele/ecal/ecalreco/multifit/studies_25ns/CMSSW_7_4_12_patch4/src/EcalReconstruction/dbscripts/ecaltemplates_popcon_runs_258287_258714.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_weekly.db -t EcalPulseShapes_data -b 258287
-conddb_import -f sqlite_file:/afs/cern.ch/work/e/emanuele/ecal/ecalreco/multifit/studies_25ns/CMSSW_7_4_12_patch4/src/EcalReconstruction/dbscripts/ecaltemplates_popcon_runs_258741_258750.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_weekly.db -t EcalPulseShapes_data -b 258741
+conddb_import -f sqlite_file:/cmshome/dimarcoe/releases/ecalreco/CMSSW_7_4_13/src/EcalReconstruction/dbscripts/ecaltemplates_popcon_runs_257615_258600.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_weekly.db -t EcalPulseShapes_data -b 257615 
+conddb_import -f sqlite_file:/cmshome/dimarcoe/releases/ecalreco/CMSSW_7_4_13/src/EcalReconstruction/dbscripts/ecaltemplates_popcon_runs_258600_259700.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_weekly.db -t EcalPulseShapes_data -b 258600
+
 
