398774712c9ae6fcb271375796cd62576ccc30db
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-04-05 10:52:52 +0200

    the 2016 legacy re-reco tag

--- dbscripts/makeThe2016Tag.sh
+++ dbscripts/makeThe2016Tag.sh
@@ -1,12 +1,15 @@
 conddb_import -f sqlite_file:/afs/cern.ch/work/e/emanuele/public/ecal/pulseshapes_db/ecaltemplates_popcon_data_Run2016B_since_271983.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 271983
 conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_data_Run2016BC_since_274958.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 274958
-conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_data_Run2016F_since_277932.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 277932
+#conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_data_Run2016F_since_277932.db -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 277932
+conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_278167_278175.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 277932
 conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_278969_278976.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 278346
 conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_279115_279116.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 278977
 conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_279653_279658.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 279474
 conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_279841_279841.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 279717 
-
-
+conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_281616_282092.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 281010 
+conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_282408_283067.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 282408
+conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_283171_283835.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 283171
+conddb_import -f sqlite_file:/afs/cern.ch/user/e/emanuele/w/public/ecal/pulseshapes_db/ecaltemplates_popcon_runs_283863_284035.db  -i EcalPulseShapes_data -c sqlite_file:ecaltemplates_popcon_2016_calib.db -t EcalPulseShapes_data -b 283863
 
 echo "===> Now checking the contents..."
 conddb --db ecaltemplates_popcon_2016_calib.db list EcalPulseShapes_data
