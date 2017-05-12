f7c0deb7929db77421082cc4b66c419af2bc54dc
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2017-05-12 09:51:52 +0200

    updates

--- scripts/mergeFiles.sh
+++ scripts/mergeFiles.sh
@@ -2,5 +2,5 @@
 
 range=$1
 echo Merging EB and EE files for range $range
-cat template_histograms_EB_runs_$range.txt template_histograms_EE_runs_$range.txt > template_histograms_ECAL_runs_$range.txt
+cat template_histograms_EB_runs_$range.txt template_histograms_EE_runs_$range.txt > template_histograms_ECAL_Run2016_runs_$range.txt
 echo Merged.
