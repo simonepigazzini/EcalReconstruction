e997ee8cdea218549eb310a0139a5938f5178c98
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2018-05-02 16:21:37 +0200

    add the accessor for counting total expected crystals for a ring

--- EcalTools/macro/EndcapAsymmetry.py
+++ EcalTools/macro/EndcapAsymmetry.py
@@ -8,10 +8,18 @@ class etaRingMapping:
     def __init__(self):
         Endc_x_y_ring=("%s/src/EcalReconstruction/data/Endc_x_y_ring.txt" % os.environ['CMSSW_BASE'])
         self.eeringmap = {}
+        self.eeringcrystals = {}
         for line in open(Endc_x_y_ring,'r'):
             item = line.split()
             key = (int(item[0]),int(item[1]))
-            self.eeringmap[key] = int(item[3])
+            # map x,y -> ring
+            thering = int(item[3])
+            self.eeringmap[key] = thering
+            # map ring -> vector of crystals
+            if not thering in self.eeringcrystals:
+                self.eeringcrystals[thering] = [(int(item[0]),int(item[1]))]
+            else:
+                self.eeringcrystals[thering].append((int(item[0]),int(item[1])))
 
         self.ebringmap = {}
         for iz in range(-1,2,2):
@@ -23,6 +31,15 @@ class etaRingMapping:
     def getRing(self,doEB,x,y,z):
         if doEB: return self.ebringmap[(x,y,z)]
         else: return self.eeringmap[(x,y)] if (x,y) in self.eeringmap else -1
+
+    def getNCrystals(self,doEB,ring):
+        if doEB: return 360
+        else: 
+            if ring==-1: return 0
+            if ring not in self.eeringcrystals: 
+                print "WARNING! Ring ",ring," not valid. Returning 0 crystals."
+                return 0
+            else: return len(self.eeringcrystals[ring])
 
 def asymmetryByRing(tv,data,doPlot=True):
 
