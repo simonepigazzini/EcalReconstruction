e0424bcd5f459dbcd0e255c500d54c3e26b495c0
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2020-01-21 17:49:01 +0100

    add the plot of the sigma of the pi0 vs BX

--- EcalTools/macro/plotPi0vsBx.C
+++ EcalTools/macro/plotPi0vsBx.C
@@ -28,6 +28,8 @@ void plotPi0vsBx() {
   double masse_err[8]={0.17,0.16,0.17,0.17,0.17,0.16,0.18,0.19} ;
   double masseM[8]={127.81,128.25,128.36,128.35,128.42,128.42,128.38,128.32} ;
   double masseM_err[8]={0.08,0.07,0.08,0.07,0.08,0.06,0.08,0.09} ;
+
+
   //  RUN E
   /*
     double masse[8]={132.698,135.576,136.887,137.612,135.991,134.837,133.529,130.688} ;
@@ -59,6 +61,7 @@ void plotPi0vsBx() {
     masseM_err[i]=masseM_err[i]/factM ;
     printf(" i %d masse[i] %f masse_err %f \n",i,masse[i],masse_err[i]) ;
   }
+  
   TGraphErrors *graph_mass ;
   graph_mass = new TGraphErrors(8,bx_num,masse,bx_num_err,masse_err) ;
   graph_mass->SetMarkerStyle(kFullCircle) ;
@@ -112,3 +115,82 @@ void plotPi0vsBx() {
   cmasse->Update() ;
   cmasse->SaveAs("pi0vsBx.pdf") ;
 }
+
+void plotPi0SigmavsBx() {
+
+  double bx_num[8]={1,2,3,4,5,6,7,8} ;
+  double bx_num_err[8]={0.001,0.001, 0.001,0.001, 0.001,0.0001,0.0001,0.0001} ;
+
+  double masse[8]={0.016763,0.016623,0.017657,0.017408,0.017059,0.017307,0.016379,0.017784};
+  double masse_err[8]={0.000345,0.000312,0.00034,0.000387,0.000417,0.000462,0.000427,0.000431};
+
+  double masseM[8]={0.011904,0.011955,0.012107,0.01211,0.012188,0.011943,0.012107,0.012041};
+  double masseM_err[8]={0.000131,0.000106,0.000119,0.000134,0.000151,0.000157,0.000164,0.000186};
+  
+  double fact= masse[0] ;
+  double factM = masseM[0] ;
+  
+  for(int i=0 ; i<8 ; i++) {
+           
+    masse[i]=masse[i] ;
+           
+    masseM[i]=masseM[i] ;
+    masse_err[i]=masse_err[i] ;
+    masseM_err[i]=masseM_err[i] ;
+    printf(" i %d masse[i] %f masse_err %f \n",i,masse[i],masse_err[i]) ;
+  }
+  
+  TGraphErrors *graph_mass ;
+  graph_mass = new TGraphErrors(8,bx_num,masse,bx_num_err,masse_err) ;
+  graph_mass->SetMarkerStyle(kFullCircle) ;
+  graph_mass->SetMarkerColor(kMagenta+2) ;
+  graph_mass->SetLineColor(kMagenta+2) ;
+  graph_mass->SetMarkerSize(1.0) ;
+  graph_mass->SetTitle("Weights") ;
+
+  TGraphErrors *graph_massM ;
+  graph_massM = new TGraphErrors(8,bx_num,masseM,bx_num_err,masseM_err) ;
+  graph_massM->SetMarkerStyle(kFullSquare) ;
+  graph_massM->SetMarkerColor(kAzure+7) ;
+  graph_massM->SetLineColor(kAzure+7) ;
+  graph_massM->SetMarkerSize(1.0) ;
+  graph_massM->SetTitle("Multi") ;
+
+  TCanvas *cmasse = new TCanvas("mass_vs_#Bx","mass_vs_#Bx", 0,0,600,600);
+  cmasse->SetLeftMargin(0.2);
+  cmasse->SetRightMargin(0.05);
+  cmasse->SetBottomMargin(0.15);
+  cmasse->SetTopMargin(0.10);
+  cmasse->cd() ;
+
+  TMultiGraph *mg_masse = new TMultiGraph("","") ;
+  mg_masse->Add(graph_mass) ;
+  mg_masse->Add(graph_massM) ;
+  mg_masse->Draw("ap") ;
+  mg_masse->GetYaxis()->SetTitle("#sigma_{m(#gamma#gamma)}") ;
+  mg_masse->GetYaxis()->SetTitleOffset(1.5);
+  mg_masse->GetYaxis()->SetDecimals(2);
+
+  mg_masse->GetXaxis()->SetTitle("bunch crossing number") ;
+  mg_masse->SetMaximum(0.022) ;
+  mg_masse->SetMinimum(0.008) ;
+  mg_masse->Draw("ap") ;
+
+  TLegend *leg = new TLegend(0.60,0.70,0.90,0.85,"","brNDC");
+  leg->SetBorderSize(0);
+  leg->AddEntry(graph_mass,"weights","pe");
+  leg->AddEntry(graph_massM,"multifit","pe");                       
+  
+  TLatex lat;
+  lat.SetNDC(); lat.SetTextFont(42); lat.SetTextSize(0.05);
+  lat.DrawLatex(0.20, 0.92, "#bf{CMS}");
+  lat.DrawLatex(0.62, 0.92, "0.5 fb^{-1} (13 TeV)");
+  lat.DrawLatex(0.35, 0.25, "ECAL Barrel");
+
+
+  leg->Draw();
+  lat.Draw();
+  cmasse->Update() ;
+  cmasse->SaveAs("pi0SigmavsBx.pdf") ;
+}
+
